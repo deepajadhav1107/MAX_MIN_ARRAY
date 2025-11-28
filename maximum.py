d7a3437dd8484eb0ee4485c7fb93395fe035ef81
Deepa Jadhav <deepajadhav2001@gmail.com>
2025-11-28 14:53:09 +0530

    Update maximum.py

--- maximum.py
+++ maximum.py
@@ -3,6 +3,10 @@ def maximum(a, b, c):
     result = max(a, b, c)
     print(f"Maximum value is: {result}")
     return result
-
+def minimum(a, b, c):
+    print(f"Input values: a={a}, b={b}, c={c}")
+    result = min(a, b, c)
+    print(f"Minimum value is: {result}")
+    return result
 if __name__ == "__main__":
     maximum(3, 9, 6)
